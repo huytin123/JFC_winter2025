e22b80aeb80794ce6858dca20427d8e79bf91618
apurva <112915195+apurrrva@users.noreply.github.com>
2025-07-14 12:30:32 +1000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import wx
-from noname import MyFrame1
+from noname2 import MyFrame1
 from typing_extensions  import override
 import fitz
 import os
@@ -8,131 +8,63 @@ import chromadb
 class MyFrame(MyFrame1):
     def __init__(self):
         super().__init__(None)
-        self.build = []
-        self.collection = []
+        self.build1 = None
+        self.dvcBuild.AppendItem(["Build 1", "Delete"])
+        self.build2 = None
+        self.dvcBuild.AppendItem(["Build 2", "Delete"])
         self.load_build(None)
         self.num = 3
+        
+    @override
+    def pdf_add( self, event ):
+        if self.build1 == None and self.build2 == None:
+            self.tc.write("Please Load a Build Before Adding PDFs\n")
+            return
 
-    
-    def get_collection(self):
-        if self.build1 != None and self.build2 != None:
-            dlg = wx.SingleChoiceDialog(self, "Choose Build", "Store PDF in Build: ", ["Build 1", "Build 2"])
-            if dlg.ShowModal() == wx.ID_CANCEL:
-                return
-
-            if dlg.GetSelection == 0:
-                collection = self.collection1
-            else:
-                collection = self.collection2
-        elif self.build1 != None:
-            collection = self.collection1
-        else:
-            collection = self.collection2
-
-        return collection
-
-    def add_folders (self):
-        pathnames = []
-        with wx.DirDialog(self, "Select a folder",
-                       style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST) as folderDialog:
-
-            if folderDialog.ShowModal() == wx.ID_CANCEL:
-                return
-
-
-            self.start_loading()
-            folder_path = folderDialog.GetPath()
-
-            #collect all files qithin the directory
-            for root, dirs, files in os.walk(folder_path):
-                for filename in files:
-                    if filename.lower().endswith('.pdf'):
-                        full_path = os.path.join(root, filename)
-                        pathnames.append(full_path)
-
-        return pathnames
-    
-    def add_files (self):
-        pathnames = []
-        with wx.FileDialog(self, "Open PDF Files", wildcard="PDF files (*.pdf)|*.pdf",
+        with wx.FileDialog(self, "Open PDF file", wildcard="PDF files (*.pdf)|*.pdf",
                        style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE) as fileDialog:
 
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return
-            
+
+            if self.build1 != None and self.build2 != None:
+                dlg = wx.SingleChoiceDialog(self, "Choose Build", "Store PDF in Build: ", ["Build 1", "Build 2"])
+                if dlg.ShowModal() == wx.ID_CANCEL:
+                    return
+
+                if dlg.GetSelection == 0:
+                    collection = self.collection1
+                else:
+                    collection = self.collection2
+            elif self.build1 != None:
+                collection = self.collection1
+            else:
+                collection = self.collection2
+
             self.start_loading()
             
             pathnames = fileDialog.GetPaths()
-        return pathnames
-       
-#     @override
-#     def pdf_add( self, event ):
-#         if len(self.build) == 0:
-#             self.tc.write("Please Load a Build Before Adding PDFs\n")
-#             return
-#         with wx.FileDialog(self, "Open PDF file", wildcard="PDF files (*.pdf)|*.pdf",
-#                        style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST | wx.FD_MULTIPLE) as fileDialog:
-
-#             if fileDialog.ShowModal() == wx.ID_CANCEL:
-#                 return
-
-#             if len(self.build) > 1:
-#                 choice = []
-#                 for i in range(len(self.build)):
-#                     choice.append("Build " + str(i + 1))
-                    
-#                 dlg = wx.SingleChoiceDialog(self, "Choose Build", "Store PDF in Build: ", choice)
-#                 if dlg.ShowModal() == wx.ID_CANCEL:
-#                     return
-
+            processed = 0
+            for pathname in pathnames:
+                name = os.path.basename(pathname)
+                docs, ids = extract_text_chunks(pathname)
+                metas = [{"Name": name}] * len(ids)
+                if not self.isSubset(collection.get(include=["metadatas"])["ids"], ids):
+                    collection.add(
+                        documents=docs,
+                        ids=ids,
+                        metadatas=metas,
+                    )
                 
-#                 collection = self.collection[dlg.GetSelection()]
-#             else:
-#                 collection = self.collection[0]
+                    self.dvc.AppendItem([name, "Delete"])
+                
+                    self.tc.write("Added: " + str(name) + "\n")
+                    processed += 1
+                    self.tc.write("Processed: " + str(processed) + "/" + str(len(pathnames)) + "\n")
+                else:
+                    self.tc.write("Document " + name + " Already in Database\n")
 
-    @override
-    def pdf_add( self, event ):
-
-        if self.build1 == None and self.build2 == None:
-            self.tc.write("Please Load a Build Before Adding PDFs\n")
-            return
-        
-        button = event.GetEventObject()
-        label = button.GetLabel() 
-
-        pathnames=[]
-        if label == "➕ Add PDF": 
-            pathnames = self.add_files()
-        else:
-            pathnames =self.add_folders()
-        if pathnames == None:
-            return 
-        
-        collection =self.collection1 # change
-        collection = self.get_collection() #change
-        if collection == None:
-            return 
-        
-        processed = 0
-        for pathname in pathnames:
-            name = os.path.basename(pathname)
-            docs, ids, metas = extract_text_chunks(pathname)
-            if not self.isSubset(collection.get(include=["metadatas"])["ids"], ids):
-                collection.add(
-                    documents=docs,
-                    ids=ids,
-                    metadatas=metas,
-                )
-            
-                self.dvc.AppendItem([name, "Delete"])
-            
-                self.tc.write("Added: " + str(name) + "\n")
-                processed += 1
-                self.tc.write("Processed: " + str(processed) + "/" + str(len(pathnames)) + "\n")
-            else:
-                self.tc.write("Document " + name + " Already in Database\n")
-
-        self.end_loading()
+            self.end_loading()
 
     def isSubset(self, a, b):
         for i in range(len(b)):
@@ -154,11 +86,17 @@ class MyFrame(MyFrame1):
         self.end_loading()
 
     def delete_item( self, name ):
-        for i in range(len(self.build)):
-            data = self.collection[i].get(include=["metadatas"])
+        if self.build1 != None:
+            data = self.collection1.get(include=["metadatas"])
             delete_id = self.find_data(data, name)
             if len(delete_id) > 0:
-                self.collection[i].delete(delete_id)
+                self.collection1.delete(delete_id)
+            
+        if self.build2 != None:
+            data = self.collection2.get(include=["metadatas"])
+            delete_id = self.find_data(data, name)
+            if len(delete_id) > 0:
+                self.collection2.delete(delete_id)
 
     def find_data( self, data, name ):
         delete_id = []
@@ -173,11 +111,16 @@ class MyFrame(MyFrame1):
 
     @override
     def pdf_fetch( self, event ):
+        if self.build1 == None and self.build2 == None:
+            self.tc.write("Please Load a Build\n")
+            return
+
         self.start_loading()
-        self.tc.write("Refreshing...\n")
         names = []
-        for i in range(len(self.build)):
-            names.extend(self.refresh_meta(self.collection[i].get(include=["metadatas"])["metadatas"]))
+        if self.build1 != None:
+            names.extend(self.refresh_meta(self.collection1.get(include=["metadatas"])["metadatas"]))
+        if self.build2 != None:
+            names.extend(self.refresh_meta(self.collection2.get(include=["metadatas"])["metadatas"]))
 
         current = []
         count = self.dvc.GetItemCount()
@@ -213,14 +156,16 @@ class MyFrame(MyFrame1):
         
     @override
     def query_search( self, event ):
-        if len(self.build) == 0:
+        if self.build1 == None and self.build2 == None:
             self.tc.write("Please Load a Build Before Searching\n")
             return
         
         text = self.text_search.GetValue()
         self.tc.write("Searched: " + text + "\n\n")
-        for i in range(len(self.build)):
-            self.query_collection(text, self.num, self.collection[i])
+        if self.build1 != None:
+            self.query_collection(text, self.num, self.collection1)
+        if self.build2 != None:
+            self.query_collection(text, self.num, self.collection2)
 
     def query_collection( self, text, n, collection):
         self.start_loading()
@@ -228,22 +173,25 @@ class MyFrame(MyFrame1):
         self.tc.write("Finished Query\n")
         self.end_loading()
         for idx in range(len(data["ids"][0])):
+            current_id = data['ids'][0][idx]
             filename = data['metadatas'][0][idx]['Name']
-            page = data['metadatas'][0][idx]['Page']
-            para = data['metadatas'][0][idx]['Paragraph']
             content = data['documents'][0][idx]
 
             self.tc.write("+" * 16 + "\n")
             self.tc.write("Top #" + str(idx+1) + "\n")
             self.tc.write("+" * 16 + "\n")
-            self.tc.write("File: " + str(filename) + "\n")
-            self.tc.write("Page: " + str(page) + "\n")
-            self.tc.write("Paragraph: " + str(para) + "\n")
+            self.tc.write(str(filename) + "\n")
+            self.tc.write(str(current_id) + "\n")
             self.tc.write("+" * 16 + "\n")
             self.tc.write(str(content) + "\n\n")
 
     @override
     def load_build( self, event ):
+        if self.build1 != None and self.build2 != None:
+            self.tc.write("Maximum Build Files Loaded\n")
+            return
+            
+        
         with wx.DirDialog (self, "Select Directory to Create or Load Build", "./",
                     wx.DD_DEFAULT_STYLE) as folder:
             
@@ -251,7 +199,7 @@ class MyFrame(MyFrame1):
                 return
             
             pathname = folder.GetPath()
-            if pathname in self.build:
+            if pathname == self.build1 or pathname == self.build2:
                 self.tc.write("Build Already Loaded\n")
                 return
 
@@ -267,10 +215,14 @@ class MyFrame(MyFrame1):
                 pass
             '''
 
-
-            self.build.append(pathname)
-            self.collection.append(chroma_client.get_or_create_collection(name="my_collection"))
-            self.dvcBuild.AppendItem(["Build " + str(len(self.build)), pathname, "Delete"])
+            if self.build1 == None:
+                self.build1 = pathname
+                self.collection1 = chroma_client.get_or_create_collection(name="my_collection")
+                self.dvcBuild.SetTextValue(pathname, 0, 1)
+            else:
+                self.build2 = pathname
+                self.collection2 = chroma_client.get_or_create_collection(name="my_collection")
+                self.dvcBuild.SetTextValue(pathname, 1, 1)
             
             self.tc.write("Loaded or Created Build: " + str(pathname) + "\n")
             self.end_loading()
@@ -281,14 +233,14 @@ class MyFrame(MyFrame1):
     def delete_build( self, event ):
         row = self.dvcBuild.ItemToRow(event.GetItem())
         name = self.dvcBuild.GetTextValue(row, 1)
-        build_num = self.dvcBuild.GetTextValue(row, 0)
-        if self.dvcBuild.GetTextValue(row, 2) == "Delete":
-            self.dvcBuild.DeleteItem(row)
-
-            self.build.pop(row)
-            self.collection.pop(row)
-
-            self.tc.write("Unloaded " + build_num + ": " + str(name) + "\n")
+        if name != "None":
+            self.dvcBuild.SetTextValue("None", row, 1)
+            if row == 0:
+                self.build1 = None
+            else:
+                self.build2 = None
+                
+            self.tc.write("Unloaded " + self.dvcBuild.GetTextValue(row, 0) + ": " + str(name) + "\n")
             self.pdf_fetch(None)
 
     def open_settings( self, event ):
@@ -301,9 +253,8 @@ class MyFrame(MyFrame1):
         self.num = result
 
     def show_help( self, event ):
-        self.tc.write("Opening User Manual: User Manual.pdf\n")
         try: 
-            os.startfile(".\\User Manual.pdf")
+            os.startfile(".\\User Manual")
         except:
             self.tc.write("Can't Open or Find User Manual\n")
 
@@ -319,7 +270,6 @@ class MyFrame(MyFrame1):
 
 def extract_text_chunks(pdf_path):
     chunk_list = []
-    id_list = []
     meta_list = []
     with fitz.open(pdf_path) as doc:
         for page_num, page in enumerate(doc):
@@ -330,16 +280,11 @@ def extract_text_chunks(pdf_path):
                 paragraph = paragraph.strip()
                 if paragraph:
                     chunk_list.append(paragraph)
-                    id_list.append(
+                    meta_list.append(
                         os.path.basename(pdf_path) +str( page_num + 1)+ str(para_count)
                     )
-                    meta_list.append({
-                        "Name": os.path.basename(pdf_path),
-                        "Page": page_num + 1,
-                        "Paragraph": para_count
-                    })
                     
-    return chunk_list, id_list, meta_list
+    return chunk_list, meta_list
 
 if __name__ == '__main__':
     app = wx.App(False)
